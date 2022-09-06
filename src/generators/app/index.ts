8363e87586ac7eb8d1f7a368c00e9a6d652d2f60
Tim Wang <tianxun.wang1@nhs.net>
2022-09-06 12:18:05 +0100

    DALA-1152 updated app generator shell scripts. added language options to app generator

--- src/generators/app/index.ts
+++ src/generators/app/index.ts
@@ -1,53 +1,82 @@
-import { BaseGenerator, shortCode } from '../../common';
+import { BaseGenerator, languages, shortCode } from '../../common';
 
 class AppGenerator extends BaseGenerator {
   constructor(args, opts) {
     super(args, opts);
 
     this._input('project', { type: 'input', validate: shortCode, store: true });
+    this._input('language', { type: 'list', choices: languages, store: true });
     this._input('region', { type: 'list', choices: ['eu-west-2'], store: true });
     this._input('state_bucket', { type: 'input', default: 'terraform-state' });
-    this._input('lock_table', { type: 'input', default: 'terraform-lock' });
+    this._input('state_lock_table', { type: 'input', default: 'terraform-lock' });
     this._input('account_id', { type: 'input' });
-    this._input('account_aws_alias', { type: 'input', default: 'mgmt' });
     this._input('account_cli_alias', { type: 'input', default: 'mgmt' });
     this._input('account_role_name', { type: 'input' });
-    this._input('assume_dev_account', {
+    this._input('dev_account_id', {
       type: 'input',
-      message: 'dev account (multi-account only)',
-      default: ({ account_id }) => account_id
+      message: 'dev account id (multi-account only)',
+      store: true
+    });
+    this._input('dev_terraform_role', {
+      type: 'input',
+      message: 'role used by terraform in dev account (multi-account only)',
+      default: 'terraform'
     });
     this._input('assume_dev_role', {
       type: 'input',
-      message: 'dev role (multi-account only)',
-      default: 'terraform'
+      message: 'assume role for dev account (multi-account only)',
+      default: 'NHSDAdminRole'
     });
-    this._input('assume_test_account', {
+    this._input('test_account_id', {
       type: 'input',
-      message: 'test account (multi-account only)',
-      default: ({ account_id }) => account_id
+      message: 'test account id (multi-account only)',
+      store: true
+    });
+    this._input('test_terraform_role', {
+      type: 'input',
+      message: 'role used by terraform in test account (multi-account only)',
+      default: 'terraform'
     });
     this._input('assume_test_role', {
       type: 'input',
-      message: 'test role (multi-account only)',
-      default: 'terraform'
+      message: 'assume role for test account (multi-account only)',
+      default: 'NHSDAdminRole'
     });
-    this._input('assume_prod_account', {
+    this._input('prod_account_id', {
       type: 'input',
-      message: 'prod account (multi-account only)',
-      default: ({ account_id }) => account_id
+      message: 'prod account id (multi-account only)',
+      store: true
+    });
+    this._input('prod_terraform_role', {
+      type: 'input',
+      message: 'role used by terraform in prod account (multi-account only)',
+      default: 'terraform'
     });
     this._input('assume_prod_role', {
       type: 'input',
-      message: 'prod role (multi-account only)',
-      default: 'terraform'
+      message: 'assume role for prod account (multi-account only)',
+      default: 'NHSDAdminRole'
     });
   }
 
   async core_application() {
     const results = await this._prompt();
 
-    this.fs.copyTpl(this.templatePath('**/*'), this.destinationRoot(), results, {}, { globOptions: { dot: true } });
+    this.fs.copyTpl(
+      this.templatePath('all/**/*.ejs'),
+      this.destinationRoot(),
+      results,
+      {},
+      { globOptions: { dot: true } }
+    );
+
+    this.fs.copyTpl(
+      this.templatePath(`${results.language}/**/*.ejs`),
+      this.destinationRoot(),
+      results,
+      {},
+      { globOptions: { dot: true } }
+    );
   }
 }
 
