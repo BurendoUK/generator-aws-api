8b54fbdf4b1917cb2ed0b0924a49fc5d3a7735c7
Rowan Gill <rowan_gill@hotmail.co.uk>
2022-09-07 17:38:51 +0100

    Added in new generator and associated templates

---
+++ src/generators/secrets/index.ts
@@ -0,0 +1,47 @@
+import { BaseGenerator, kebabCase, listKmsKeys } from '../../common';
+
+
+class SecretsGenerator extends BaseGenerator {
+  constructor(args, opts) {
+    super(args, opts);
+
+    this._input('name', { type: 'input', validate: kebabCase });
+    this._input('description', { type: 'input' });
+    this._input('secret_string', { type: 'input' });
+    this._input('repeat', { type: 'confirm', message: 'Do you want to add another secret?' });
+    
+  }
+
+  promptUser = async () => {
+    return this._prompt()
+  }
+
+  async execute() {
+    let answers = [];
+    let repeat = 'true'
+
+    while (repeat === 'true') {
+        let result = await this.promptUser()
+        answers.push(result)
+
+        if(result.repeat === false) {
+            repeat = 'false'
+        }
+    }
+
+    console.log(answers)
+
+
+    this.fs.copyTpl(
+      this.templatePath('**/*.ejs'),
+      this.destinationRoot(),
+      {data: answers},
+      {},
+      {
+        globOptions: { dot: true }
+      }
+    );
+  }
+}
+
+module.exports = SecretsGenerator;
