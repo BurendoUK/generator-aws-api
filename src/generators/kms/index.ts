88bbb9590f7748c708731cd54f4eb5a372b923f1
Tim Wang <tianxun.wang1@nhs.net>
2022-09-12 16:28:37 +0100

    DALA-1183 enabled kms cloudwatch encryption

--- src/generators/kms/index.ts
+++ src/generators/kms/index.ts
@@ -1,4 +1,4 @@
-import { BaseGenerator, kebabCase } from '../../common';
+import { BaseGenerator, BOOLEAN_CHOICES, kebabCase, NO } from '../../common';
 import * as path from 'path';
 
 class KmsKeyGenerator extends BaseGenerator {
@@ -7,6 +7,8 @@ class KmsKeyGenerator extends BaseGenerator {
 
     this._input('name', { type: 'input', validate: kebabCase });
     this._input('description', { type: 'input' });
+    this._input('region', { type: 'list', choices: ['eu-west-2'], store: true });
+    this._input('is_cloudwatch', { type: 'list', choices: BOOLEAN_CHOICES, default: NO });
   }
 
   async generate() {
